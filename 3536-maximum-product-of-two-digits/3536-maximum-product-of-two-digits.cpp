62d5e4dd9a2283b602128434b4d606a526134f48
Suryansh <reachoutsuryansh@gmail.com>
2026-08-05 00:12:33 +0530

    Time: 2 ms (14.36%), Space: 8.9 MB (40.01%) - LeetHub - Time: O((log n)^2), Space: O(log n)

---
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxProduct(int n) {
+        vector<int> digits;
+
+        while (n > 0) {
+            digits.push_back(n % 10);
+            n /= 10;
+        }
+        int ans = 0;
+        for (int i = 0; i < digits.size(); i++) {
+            for (int j = i + 1; j < digits.size(); j++) {
+                ans = max(ans, digits[i] * digits[j]);
+            }
+        }
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
