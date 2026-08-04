4ff9060db0201e6f91ef32ca13e4a4388b058710
Suryansh <reachoutsuryansh@gmail.com>
2026-08-04 23:49:42 +0530

    Time: 0 ms (100%), Space: 32.2 MB (91.09%) - LeetHub - Time: O(?), Space: O(?)

---
+++ 3731-find-missing-elements/3731-find-missing-elements.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> findMissingElements(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<int> ans;
+        for (int i = 0; i < nums.size() - 1; i++) {
+            for (int j = nums[i] + 1; j < nums[i + 1]; j++) {
+                ans.push_back(j);
+            }
+        }
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
